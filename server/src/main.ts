ceb3d87f1fe7471740667df4e499d463de8945c8
mugifly <mp@ohgita.info>
2022-04-14 02:03:43 +0900

    feat(server): OpenAPI document with using Swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); // Change the URL prefix to `/api` on backend
+
+  // Change the URL prefix to `/api` on backend
+  app.setGlobalPrefix('api');
+
+  // Build the OpenAPI document (published under `/api/docs`) with Swagger
+  const doc_options = new DocumentBuilder().setTitle(`API Document`).build();
+  const doc = SwaggerModule.createDocument(app, doc_options);
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+    // Enable Swagger UI for development env
+    SwaggerModule.setup('api/docs', app, doc);
+  }
+
+  // Start the server
   await app.listen(3000);
 }
 bootstrap();
