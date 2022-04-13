f34d90af184c74264f01c37b217a3c17825fdf14
mugifly <mp@ohgita.info>
2022-04-14 01:45:48 +0900

    chore: Production - Serve frontend as a static files through backend
    This structure is the opposite of the development environment that is using proxy by frontend (701dce4).
    
    Execution steps:
    
    1. Build the frontend code and the backend code to `dist` directory: `npm run build`
    
    2. Run the backend server: `npm start`
    
    Now, it looks like this:
    
    http://localhost:3000/ -> Frontend (Angular app) that served as static through Backend.
    http://localhost:3000/api -> Backend (NestJS app)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Serve the frontend (Angular) app (../../client/dist/client/) as a static files
+    // (for production environment)
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client', 'dist', 'client'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
