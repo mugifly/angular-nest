701dce4392e15bcc2da26a46a16469935848f829
mugifly <mp@ohgita.info>
2022-04-13 22:38:25 +0900

    chore: Development - Communicate with backend through proxy by frontend (ng serve)
    http://localhost:4200/ -> Frontend (Angular app)
    http://localhost:4200/api -> Backend (NestJS app)

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // Change the URL prefix to `/api` on backend
   await app.listen(3000);
 }
 bootstrap();
