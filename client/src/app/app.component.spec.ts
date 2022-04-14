d083bff1155deb0e465d351ca3a7b0000e1c4d95
mugifly <mp@ohgita.info>
2022-04-14 17:36:54 +0900

    test: Fix broken test

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -5,7 +6,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -16,18 +17,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
-  });
-
-  it('should render title', () => {
+  it('should render card', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'client app is running!',
+    expect(compiled.querySelector('mat-card-title')?.textContent).toContain(
+      'What is this?',
     );
   });
 });
--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return string with "Hello World"', () => {
+      expect(appController.getHello()).toContain('Hello World');
     });
   });
 });
