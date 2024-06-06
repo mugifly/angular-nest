4d5f2dbe0f29c4db1fa2bd301b081830459323af
mugifly <mp@ohgita.info>
2024-06-07 01:24:00 +0900

    chore(client): Update to files generated by Angular 18 CLI

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { environment } from './environments/environment';
+import { environment } from './environments/environment.development';
 import { AppComponent } from './app/app.component';
 import { ApiModule } from 'src/.api-client/api.module';
 import {
@@ -10,10 +10,6 @@ import {
 } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [
