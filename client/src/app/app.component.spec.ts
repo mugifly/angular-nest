7bd7ed5d9edd0015d75da483498ccac87b9c4775
mugifly <mp@ohgita.info>
2024-06-07 00:23:58 +0900

    chore(client): Update to AppComponent generated by Angular 18 CLI

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,25 +1,31 @@
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterModule, provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [RouterModule, AppComponent],
-    providers: [
-        provideHttpClient(withInterceptorsFromDi()),
-        provideRouter([]),
-    ],
-}).compileComponents();
+      imports: [AppComponent],
+    }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'client' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('client');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, client',
+    );
   });
 });
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [
-        NgSwitch,
-        NgSwitchDefault,
-        NgSwitchCase,
-        RouterOutlet,
-    ],
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
