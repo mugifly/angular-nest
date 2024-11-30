9e5162ff792ed44a7037c996ffbd27e546184a21
mugifly <mp@ohgita.info>
2024-11-30 11:11:01 +0900

    chore: Update Angular 18 -> 19, TypeScript 5.4 -> 5.6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
